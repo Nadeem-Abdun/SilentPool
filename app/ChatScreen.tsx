90e944d3bb410697236b670da6b1a72d195586a1
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-04-04 20:16:03 +0530

    UI Updated And Socket Notification Event Added

--- app/ChatScreen.tsx
+++ app/ChatScreen.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, TextInput } from 'react-native';
-import { Button, Menu } from 'react-native-paper';
+import { View, Text, StyleSheet, FlatList, TextInput, TouchableWithoutFeedback, Share } from 'react-native';
+import { IconButton, Drawer } from 'react-native-paper';
 import { RootStackParamList } from '@/types/navigation';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
-import { GetMessages, PostLeavePool } from '@/services/serviceControllers';
+import { GetMessages, GetPoolInfo, PostLeavePool } from '@/services/serviceControllers';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { io } from 'socket.io-client';
 import { IMessage } from '@/types/message';
 import { getSessionInfo } from '@/utilities/sessionsHandler';
 import { WEBSOCKET_URL } from '@/services/endPoints';
 import { decryptMessage } from '@/utilities/encryption';
+import NotificationPill from '@/components/NotificationPill';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 type ChatScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Chat'>;
 type ChatScreenRouteProp = RouteProp<RootStackParamList, 'Chat'>;
@@ -21,12 +23,11 @@ export default function ChatScreen() {
     const senderAlias = getSessionInfo()?.alias;
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState<IMessage[]>([]);
+    const [participants, setParticipants] = useState<string[]>([]);
     const [socket, setSocket] = useState<any>(null);
-    const [isTyping, setIsTyping] = useState({
-        status: false,
-        alias: ''
-    });
-    const [menuVisible, setMenuVisible] = useState(false);
+    const [isTyping, setIsTyping] = useState({ status: false, alias: '' });
+    const [drawerVisible, setDrawerVisible] = useState(false);
+    const [notifications, setNotifications] = useState<string | null>(null);
     let typingTimeout: NodeJS.Timeout | null = null;
 
     // Websocket Initialization
@@ -58,6 +59,10 @@ export default function ChatScreen() {
             typingTimeout = setTimeout(() => {
                 setIsTyping({ status: false, alias: '' });
             }, 3000);
+        });
+
+        newSocket.on('notification', (notificationEvent) => {
+            setNotifications(notificationEvent);
         });
 
         newSocket.on('disconnect', () => {
@@ -95,11 +100,31 @@ export default function ChatScreen() {
             console.error('Failed to leave pool', error);
         }
     };
+    const getPoolInfoApiCall = async () => {
+        try {
+            const response = await GetPoolInfo(poolId);
+            if (response) {
+                setParticipants(response.participants);
+            }
+        } catch (error) {
+            console.error('Failed to get pool info', error);
+        }
+    };
 
     // Websocket Event Emmitters
     const pushJoinPoolEventToSocket = (socketInstance: any) => {
         if (socketInstance) {
             socketInstance.emit('join', { poolId, senderAlias });
+        }
+    };
+    const pushLeavePoolEventToSocket = () => {
+        if (socket) {
+            socket.emit('leave', { poolId, senderAlias });
+        }
+    };
+    const pushLeavePoolQuietlyEventToSocket = () => {
+        if (socket) {
+            socket.emit('leave_quietly', { poolId, senderAlias });
         }
     };
     const pushMessageEventToSocket = () => {
@@ -134,30 +159,100 @@ export default function ChatScreen() {
         const response = await leavePoolApiCall();
         if (response) {
             navigation.navigate('Home');
+            pushLeavePoolEventToSocket();
         } else {
             navigation.navigate('Error', { message: 'Failed to leave pool. Please try again!' });
         }
     };
+    const handleLeavePoolQuietly = async () => {
+        const response = await leavePoolApiCall();
+        if (response) {
+            navigation.navigate('Home');
+            pushLeavePoolQuietlyEventToSocket();
+        } else {
+            navigation.navigate('Error', { message: 'Failed to leave pool. Please try again!' });
+        }
+    };
+    const handleCopyPoolId = () => {
+        setDrawerVisible(false);
+        Clipboard.setString(poolId);
+        setNotifications('Pool ID copied to clipboard.');
+    };
+    const handleSharePoolId = async () => {
+        setDrawerVisible(false);
+        Share.share({ message: `Join my pool on SilentPool! Use this Pool ID: ${poolId}` });
+    };
 
     useEffect(() => {
         getMessagesApiCall();
+        const interval = setInterval(() => {
+            getPoolInfoApiCall();
+        }, 15000);
+        return () => clearInterval(interval);
     }, []);
     return (
         <View style={styles.container}>
+            {/* Drawer */}
+            {drawerVisible && (
+                <TouchableWithoutFeedback onPress={() => setDrawerVisible(false)}>
+                    <View style={styles.overlay}>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.drawerContainer}>
+                                <Drawer.Section title="Chat Options">
+                                    <Drawer.Item
+                                        label="Copy Pool Id"
+                                        icon="content-copy"
+                                        onPress={handleCopyPoolId}
+                                    />
+                                    <Drawer.Item
+                                        label="Share Pool Id"
+                                        icon="share"
+                                        onPress={handleSharePoolId}
+                                    />
+                                    <Drawer.Item
+                                        label="Leave Pool"
+                                        icon="exit-to-app"
+                                        onPress={handleLeavePool}
+                                    />
+                                    <Drawer.Item
+                                        label="Leave Quietly"
+                                        icon="ghost"
+                                        onPress={handleLeavePoolQuietly}
+                                    />
+                                </Drawer.Section>
+                                <Drawer.Section title="Participants">
+                                    <FlatList
+                                        data={participants}
+                                        renderItem={({ item }) => {
+                                            return (
+                                                <View style={styles.participantContainer}>
+                                                    <Text style={styles.participantName}>{item}</Text>
+                                                    <IconButton icon="circle" size={10} iconColor='green' />
+                                                </View>
+                                            );
+                                        }}
+                                        keyExtractor={(_, index) => index.toString()}
+                                        style={styles.participantsContainer}
+                                    />
+                                </Drawer.Section>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </TouchableWithoutFeedback>
+            )}
+
+            {/* Chat Screen Header with Drawer Button */}
             <View style={styles.header}>
-                <Text style={styles.headerText}>Pool ID: {poolId}</Text>
-                <Menu
-                    visible={menuVisible}
-                    onDismiss={() => setMenuVisible(false)}
-                    anchor={
-                        <Button icon="dots-vertical" onPress={() => setMenuVisible(true)} children={undefined} style={styles.menuButton} />
-                    }
-                    mode='elevated'
-                    style={styles.menuContent}
-                >
-                    <Menu.Item onPress={() => handleLeavePool} title="Leave" />
-                </Menu>
+                <IconButton
+                    icon="menu"
+                    iconColor="#fff"
+                    size={20}
+                    onPress={() => setDrawerVisible(!drawerVisible)}
+                />
+                <Text style={styles.headerText}>Pool {poolId}</Text>
             </View>
+
+            {/* Chat Messages */}
             <FlatList
                 data={messages}
                 renderItem={({ item }) => {
@@ -179,9 +274,18 @@ export default function ChatScreen() {
                 keyExtractor={(_, index) => index.toString()}
                 style={styles.chatLog}
             />
+
+            {/* Notification Indicator */}
+            {notifications && (
+                <NotificationPill message={notifications} onDismiss={() => setNotifications(null)} />
+            )}
+
+            {/* Typing Indicator */}
             {isTyping.status && (
                 <Text style={styles.typingIndicator}>{isTyping.alias} is typing...</Text>
             )}
+
+            {/* Input Field */}
             <View style={styles.inputContainer}>
                 <TextInput
                     style={styles.input}
@@ -189,7 +293,7 @@ export default function ChatScreen() {
                     value={message}
                     onChangeText={handleMessageChange}
                 />
-                <Button icon='send' onPress={handleSendMessage} mode='text' contentStyle={{ flexDirection: 'row-reverse' }}>Send</Button>
+                <IconButton icon='send' onPress={handleSendMessage} iconColor='#00DDEC' />
             </View>
         </View>
     );
@@ -198,27 +302,61 @@ export default function ChatScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#f7f7f7',
+        backgroundColor: '#151718',
+    },
+    drawerContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '70%',
+        height: '100%',
+        backgroundColor: '#fff',
+        shadowColor: '#000',
+        shadowOffset: { width: 2, height: 0 },
+        shadowOpacity: 0.1,
+        elevation: 10,
+        zIndex: 1,
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        zIndex: 1,
+    },
+    participantsContainer: {
+        flex: 1,
+        maxHeight: 300,
+        minHeight: 100,
+        paddingHorizontal: 10,
+    },
+    participantContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        marginBottom: 10,
+        backgroundColor: '#f0f0f0',
+    },
+    participantName: {
+        fontSize: 13,
+        color: 'green',
     },
     header: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         height: 50,
-        padding: 10,
-        backgroundColor: '#007AFF',
+        paddingVertical: 10,
+        paddingRight: 10,
+        backgroundColor: '#00DDEC',
     },
     headerText: {
-        color: '#fff',
         fontSize: 18,
+        color: '#fff',
         fontWeight: 'bold',
-    },
-    menuContent: {
-        padding: 0,
-        margin: 0,
-    },
-    menuButton: {        
-        alignSelf: 'flex-end',
     },
     chatLog: {
         flex: 1,
@@ -229,22 +367,25 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         paddingHorizontal: 10,
         paddingVertical: 5,
-        backgroundColor: '#eaeaea',
         maxWidth: '90%',
     },
     senderName: {
         fontSize: 12,
-        color: '#900',
+        color: '#fff',
         fontWeight: 'bold',
     },
     message: {
         fontSize: 16,
+        color: '#fff',
+        fontWeight: 'medium',
     },
     leftMessage: {
         alignSelf: 'flex-start',
+        backgroundColor: '#3A3C3D',
     },
     rightMessage: {
         alignSelf: 'flex-end',
+        backgroundColor: '#00C4D2',
     },
     typingIndicator: {
         fontSize: 14,
@@ -266,7 +407,8 @@ const styles = StyleSheet.create({
         borderColor: '#ddd',
         borderRadius: 8,
         paddingHorizontal: 10,
-        backgroundColor: '#fff',
+        color: '#fff',
+        backgroundColor: '#555',
         marginRight: 5,
     },
 });
